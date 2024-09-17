e2c411c9f4a05844598b05fd67bda147af299684
Eli-2009 <elieliyarov18@gmail.com>
2024-09-17 18:22:18 +0400

    homework

---
+++ app.js
@@ -0,0 +1,9 @@
+let arr = ["qırmızı", "mavi", "yaşıl", "sarı"]
+arr.pop()
+arr.push('qara')
+arr.shift()
+arr.unshift('narinci')
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
